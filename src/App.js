e1487a3bef5d9173950d530cc46a062e3bbb2ceb
HDS Tech <hdstech@HDSs-MacBook-Pro.local>
2020-11-28 16:03:13 -0500

    git initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { PuffLoader } from 'react-spinners';
 import './App.css';
 
 function App() {
+  const [selectedMonth, setSelectedMonth] = useState('')
+  const [isSelecting, setIsSelecting] = useState(false);
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November"]
+  const range = months.length;
+
+  const selectMonthClick = () => {
+    setIsSelecting(true)
+    setSelectedMonth('')
+    setTimeout(() => {
+      const randomMonth = getRandomMonth();
+      setSelectedMonth(randomMonth);
+      setIsSelecting(false);
+    }, 1500);
+  }
+
+  const getRandomMonth = () => {
+    const index = Math.floor(Math.random() * (range - 1))
+    return months[index];
+  }
+
   return (
     <div className="App">
+      <div className="top-message">
+        <p>Pick a month and remember what you're grateful for.</p>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="selected-month">
+          {isSelecting ?
+            <PuffLoader color="#d029d2" /> :
+            <p>
+              {selectedMonth}
+            </p>
+          }
+        </div>
+        <button type="button" onClick={selectMonthClick}><span>Choose your month</span></button>
       </header>
     </div>
   );
